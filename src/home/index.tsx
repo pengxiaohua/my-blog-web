1c7ca6ef0927c6a92ba4faf8d9aa2fb144b18a73
彭小华 <pengxiaohua@xiaomi.com>
2022-08-06 23:57:33 +0800

    init project

---
+++ src/home/index.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from "react"
+
+import './index.scss'
+
+const HomePage: FC = () => {
+  return (
+    <div className="home-age">
+        <h1 className="home-age__title">THIS IS MY BLOG~</h1>
+        <p>http://www.xiaohua.life</p>
+    </div>
+  )
+}
+
+export default HomePage
