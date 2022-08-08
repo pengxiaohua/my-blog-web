28c0a0f5f1f11a8d2198fffc582792909861ee8e
xiaohua <609370075@qq.com>
2022-08-08 23:57:03 +0800

    feat: 修改个人首页

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -7,6 +7,11 @@ const HomePage: FC = () => {
     <div className="home-age">
         <h1 className="home-age__title">THIS IS MY BLOG~</h1>
         <p>http://www.xiaohua.life</p>
+        <h3>首页</h3>
+        <h3>技术</h3>
+        <h3>随想</h3>
+        <h3>读书笔记</h3>
+        <h3>关于我</h3>
     </div>
   )
 }
