ab8c882ee851580eafc5adda5a01a47bda0370c6
xiaohua <609370075@qq.com>
2022-08-09 23:10:07 +0800

    feat: 引入sass样式写法

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,11 +1,10 @@
 import React, {FC} from "react"
-
 import './index.scss'
 
 const HomePage: FC = () => {
   return (
-    <div className="home-age">
-        <h1 className="home-age__title">THIS IS MY BLOG~</h1>
+    <div className="home">
+        <h1 className="home__title">THIS IS MY BLOG~</h1>
         <p>http://www.xiaohua.life</p>
         <h3>首页</h3>
         <h3>技术</h3>
