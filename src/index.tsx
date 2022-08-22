559b4e1d8d27f07ebd87f69fc6e0c00a97693893
xiaohua <609370075@qq.com>
2022-08-22 23:59:16 +0800

    feat: delete useless codes

--- src/index.tsx
+++ src/index.tsx
@@ -11,9 +11,6 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <React.StrictMode>
-  //   <Home />
-  // </React.StrictMode>
   <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
