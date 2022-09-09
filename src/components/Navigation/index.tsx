5ff57d3d0867f345d69b653c41ad9d5a9fdabdb1
xiaohua <609370075@qq.com>
2022-09-09 21:18:36 +0800

    feat: 修改导航静态数据

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react"
 import { Link } from "react-router-dom";
 
+import { navList } from "../../utils/common";
 import './index.scss'
 
 interface IProps { }
@@ -15,24 +16,13 @@ const Navigation: FC<Props> = (props) => {
   return (
     <div className="navigation-page">
       <ul className="navigation-page__nav-list font-bold dark:text-white text-[#0B1120]">
-        <li className="navigation-page__nav-item">
-          <Link to="/" className="hover:text-sky-500 dark:hover:text-sky-400">首页</Link>
-        </li>
-        <li className="navigation-page__nav-item">
-          <Link to="/tech" className="hover:text-sky-500 dark:hover:text-sky-400">技术</Link>
-        </li>
-        <li className="navigation-page__nav-item">
-          <Link to="/essays" className="hover:text-sky-500 dark:hover:text-sky-400">随笔</Link>
-        </li>
-        <li className="navigation-page__nav-item">
-          <Link to="/reading" className="hover:text-sky-500 dark:hover:text-sky-400">读书</Link>
-        </li>
-        <li className="navigation-page__nav-item">
-          <Link to="/thinking" className="hover:text-sky-500 dark:hover:text-sky-400">随想</Link>
-        </li>
-        <li className="navigation-page__nav-item">
-          <Link to="/about-me" className="hover:text-sky-500 dark:hover:text-sky-400">关于我</Link>
-        </li>
+        {
+          navList.map(nav => (
+            <li className="navigation-page__nav-item" key={nav.key}>
+              <Link to={nav.route} className="hover:text-sky-500 dark:hover:text-sky-400">{nav.title}</Link>
+            </li>
+          ))
+        }
       </ul>
     </div>
   )
---
+++ src/utils/common.ts
@@ -0,0 +1,33 @@
+// 导航数据
+export const navList = [
+  {
+    title: '首页',
+    key: 'home',
+    route: '/'
+  },
+  {
+    title: '技术',
+    key: 'tech',
+    route: '/tech'
+  },
+  {
+    title: '随笔',
+    key: 'essays',
+    route: '/essays'
+  },
+  {
+    title: '读书',
+    key: 'reading',
+    route: '/reading'
+  },
+  {
+    title: '随想',
+    key: 'thinking',
+    route: '/thinking'
+  },
+  {
+    title: '关于我',
+    key: 'aboutMe',
+    route: '/about-me'
+  },
+]
