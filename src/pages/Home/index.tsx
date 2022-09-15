d5a56b807aa3568bd97431d445c82fa34050b88d
xiaohua <609370075@qq.com>
2022-09-15 23:59:42 +0800

    feat: 写入mock数据

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react"
 
 import Header from "../../components/Header"
 import Footer from '../../components/Footer'
+import { blogList } from "../../mock/blog-list"
 import './index.scss'
 
 const HomePage: FC = () => {
@@ -10,7 +11,19 @@ const HomePage: FC = () => {
       <Header />
       <div className="home__content">
         <article className="home__blog-list">
-          BLOG-LIST
+          {
+            blogList.map(blog => (
+              <div className="home__blog-item">
+                <div className="home__blog-item__title">{blog.title}</div>
+                <div className="home__blog-item__time-and-author">
+                  <div className="home__blog-item__time">{blog.createTime}</div>
+                  <div className="home__blog-item__author">{blog.author}</div>
+                </div>
+                <div className="home__blog-item__content">{blog.content}</div>
+                <div className="home__blog-item__tags">{blog.tags}</div>
+              </div>
+            ))
+          }
         </article>
         <div className="home__blog-aside">BLOG-ASIDE</div>
       </div>
