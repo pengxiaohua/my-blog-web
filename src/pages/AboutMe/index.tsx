4a1dd39dc962c46f3bd510480660c1088482872c
xiaohua <609370075@qq.com>
2022-09-29 23:55:22 +0800

    feat: 增加个人简介内容

--- src/pages/AboutMe/index.tsx
+++ src/pages/AboutMe/index.tsx
@@ -16,6 +16,10 @@ const AboutMe: FC<Props> = (props) => {
     <div className="about-me">
       <Navigation />
       <h1>THIS IS ABOUT ME！</h1>
+      <h3>小米(武汉)</h3>
+      <h3>高德地图</h3>
+      <h3>小米(北京)</h3>
+      <h3>金风科技</h3>
     </div>
   )
 }
