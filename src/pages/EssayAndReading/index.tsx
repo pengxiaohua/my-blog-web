eae5c08ccee6556fbfd55b2da609a956a91b1991
xiaohua <609370075@qq.com>
2022-10-16 23:55:06 +0800

    feat: 完善随笔和读书页面

--- src/pages/EssayAndReading/index.tsx
+++ src/pages/EssayAndReading/index.tsx
@@ -1,6 +1,9 @@
 import React, {FC} from "react"
 
 import ContentWrap from "../../components/ContentWrap"
+import { blogList } from "../../mock/blog-list"
+import BlogItem from "../../components/BlogItem"
+import Aside from "../../components/Aside"
 import './index.scss'
 
 interface IProps {}
@@ -14,7 +17,14 @@ type Props = IProps & Partial<DefaultProps>
 const EssayAndReading: FC<Props> = (props) => {
   return (
     <div className="essay-and-reading">
-      <ContentWrap>EssayAndReading</ContentWrap>
+      <ContentWrap flex>
+        <article className="home__blog-list">
+          {
+            blogList.map(blog => <BlogItem blog={blog} key={blog.id} />)
+          }
+        </article>
+        <Aside />
+      </ContentWrap>
     </div>
   )
 }
