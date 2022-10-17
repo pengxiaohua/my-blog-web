fe4ea0b38c3cab58118453c3ad1d94754ff2355c
xiaohua <609370075@qq.com>
2022-10-17 20:53:41 +0800

    feat: 完善随想页面

--- src/pages/Thinking/index.tsx
+++ src/pages/Thinking/index.tsx
@@ -1,6 +1,9 @@
 import React, {FC} from "react"
 
 import ContentWrap from "../../components/ContentWrap"
+import { blogList } from "../../mock/blog-list"
+import BlogItem from "../../components/BlogItem"
+import Aside from "../../components/Aside"
 import './index.scss'
 
 interface IProps {}
@@ -14,7 +17,14 @@ type Props = IProps & Partial<DefaultProps>
 const Thinking: FC<Props> = (props) => {
   return (
     <div className="thinking-page">
-      <ContentWrap>THINKING</ContentWrap>
+      <ContentWrap flex>
+        <article className="home__blog-list">
+          {
+            blogList.map(blog => <BlogItem blog={blog} key={blog.id} />)
+          }
+        </article>
+        <Aside />
+      </ContentWrap>
     </div>
   )
 }
