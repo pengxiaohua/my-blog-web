69a3a33cd478681b4111bd67882e502938bcd0e5
xiaohua <609370075@qq.com>
2022-10-19 22:58:44 +0800

    feat: 完善个人简介页面

--- src/pages/AboutMe/index.tsx
+++ src/pages/AboutMe/index.tsx
@@ -20,6 +20,7 @@ const AboutMe: FC<Props> = (props) => {
         <h3>小米(北京)</h3>
         <h3>金风科技</h3>
         <h3>搜房网</h3>
+        <h3>武汉科技大学-信息管理与信息系统</h3>
       </ContentWrap>
     </div>
   )
